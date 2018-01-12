17df777de459e0b467ed2736d19acca09e5ade82
Kevin Burnham <kevin.burnham@pilytix.com>
2018-01-12 14:41:16 -0600

    switch to csv for change tracking, make master log instead of separate

--- app.R
+++ app.R
@@ -10,22 +10,18 @@
 library(shiny)
 library(tidyverse)
 library(lubridate)
-
+library(zoo)
 users <- list("BK" = list(name = "kevin",
-                             target = 195,
-                             weighins = data.frame()
+                             target = 195
                             ),
               "NoushDog" = list(name = "anousha",
-                               target = 110,
-                               weighins = data.frame()
+                               target = 110
               ),
               "RoryBoo" = list(name = "rory",
-                            target = 155,
-                            weighins = data.frame()
+                            target = 155
                             ),
               "Snowzy" = list(name = "Snowden",
-                               target = 142,
-                               weighins = data.frame()
+                               target = 142
                                )
               
               )
@@ -33,28 +29,45 @@ users <- list("BK" = list(name = "kevin",
 
 
 ## make each log a separate rds object named after its user
-load_log <- function(person) {
-  log <- readRDS(paste0(person, "_log.rds"))
+load_log <- function(user = NA) {
+  log <- read_csv("log.csv")
+  if (is.na(user)) return(log) else return(log %>% filter(person == user))
 }
+
+
+# names(omni_log) <- names(users)
+# log <- bind_rows(omni_log, .id = 'person')
+# saveRDS(log, "log.RDS")
+
+
+
+#add_to_log("kevin", as.Date("2018-01-21"), 50)
 
 add_to_log <- function(person, date, weight) {
-  input <- data.frame(date = as.Date(date), weight = weight)
-  log <- load_log(person)
+  input <- data.frame(person = person, date = as.Date(date), weight = weight)
+  log <- load_log()
   log <- rbind(log, input)
-  saveRDS(log, paste0(person, "_log.rds"))
+  write_csv(log, "log.csv")
 }
 
-
+delete_from_log <- function(user, Date, Weight) {
+  log <- load_log() %>% 
+    filter(person != user & date != Date & weight != Weight)
+  
+  write_csv(log, "log.csv")
+  
+}
+delete_from_log("BK", as.Date("2018-01-12"), 500)
 
 
 ## this function takes in a date, weight df and outputs rolling average and other data as required
-process_log <- function(log, days_to_average = 7) {
+process_log <- function(log, who, days_to_average = 7) {
   ## add to front of log so that rollmean and roll sum will always work
-  append <- data.frame(date = seq(min(log$date) - days(7), min(log$date) - days(1),
+  log <- log %>% filter(person == who)
+  append <- data.frame(person = who, date = seq(min(log$date) - days(8), min(log$date) - days(1),
                                   by = "days"),
                        weight = log$weight[log$date == min(log$date)])
   log <- rbind(append, log)
-  
   
   log %>% 
     mutate(source = "scale") %>% 
@@ -95,22 +108,25 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  # log <- eventReactive(input$person, {
+  #   return(load_log(input$person))
+  # })
    
   log <- eventReactive(input$add_weight, {
     if (!is.na(input$weight)) {
       add_to_log(person = input$person, date = input$date, weight = input$weight)
-      new <- load_log(input$person)
+      new <- load_log()
       return(new)
     } else {
-      return(load_log(input$person))
+      return(load_log())
     }}, ignoreNULL = FALSE)
   
   
-  log <- eventReactive(input$person, {
-    return(load_log(input$person))
-  })
-
-   output$log <- renderDataTable({log()})
+ 
+   output$log <- renderDataTable({log() %>% process_log(input$person) %>% 
+       filter(date > as.Date("2018-01-08") & source == "scale") %>% 
+       select(date, weight, moving_average, daily_weight_loss, weight_loss_past_7_days)})
   }
 
 
